3f406d552550294446d2a50fa4012c2380ddda46
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-01-18 22:45:20 +0500

    feat: task done

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const stylesDir = path.join(__dirname, 'styles');
+const projectDir = path.join(__dirname, 'project-dist');
+
+const bundleName = 'bundle.css';
+
+fs.readdir(stylesDir, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    throw 'Ошибка чтения файлов';
+  }
+  files.forEach((file) => {
+    if (file.isFile() && path.extname(file.name).slice(1) === 'css') {
+      const readStream = fs.createReadStream(
+        path.join(stylesDir, file.name),
+        'utf-8',
+      );
+      readStream.on('data', (chunk) =>
+        fs.appendFile(path.join(projectDir, bundleName), chunk, (err) => {
+          if (err) {
+            throw 'Ошибка записи bundle.css';
+          }
+        }),
+      );
+    }
+  });
+});
