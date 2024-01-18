78188876abbfe2c9fba265c53b040fecd9f02350
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-01-18 21:56:13 +0500

    fix: add throwing errors

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,10 +5,16 @@ const dirName = 'secret-folder';
 const dirPath = path.join(__dirname, dirName);
 
 fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    throw 'Ошибка чтения файлов';
+  }
   files.forEach((file) => {
     if (file.isFile()) {
       const filePath = path.join(dirPath, file.name);
       fs.stat(filePath, (err, stats) => {
+        if (err) {
+          throw 'Ошибка чтения параметров файлов';
+        }
         const fileName = file.name;
         const fileExtension = path.extname(filePath).slice(1);
         const fileSize = stats.size;
