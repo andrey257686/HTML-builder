0b1e929e181ba4e3c2fdadb390447fe1bfde506b
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-01-18 22:13:10 +0500

    feat: task done

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const filesDir = path.join(__dirname, 'files');
+const copyDir = path.join(__dirname, 'files-copy');
+
+fs.mkdir(copyDir, { recursive: true }, (err) => {
+  if (err) {
+    throw 'Ошибка создания директории';
+  }
+  fs.readdir(filesDir, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      throw 'Ошибка чтения файлов';
+    }
+    files.forEach((file) => {
+      if (file.isFile()) {
+        fs.copyFile(
+          path.join(filesDir, file.name),
+          path.join(copyDir, file.name),
+          (err) => {
+            if (err) {
+              throw 'Ошибка копирования файлов';
+            }
+          },
+        );
+      } else if (file.isDirectory()) {
+        fs.cp(
+          path.join(filesDir, file.name),
+          path.join(copyDir, file.name),
+          {
+            recursive: true,
+          },
+          (err) => {
+            if (err) {
+              throw 'Ошибка копирования директории';
+            }
+          },
+        );
+      }
+    });
+  });
+});
